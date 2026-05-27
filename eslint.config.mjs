6688e8562cdbdd1a18de13183c6ded1055baadaf
terencedlewis <terencelewis@itechgroupllc.com>
2026-05-27 05:22:19 -0400

    chore: ignore build artifacts in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
     baseDirectory: __dirname
 });
 
-const config = [...compat.extends("next/core-web-vitals")];
+const config = [
+    {
+        ignores: [".next/**", "out/**", "build/**", "coverage/**", "node_modules/**"]
+    },
+    ...compat.extends("next/core-web-vitals")
+];
 
 export default config;
